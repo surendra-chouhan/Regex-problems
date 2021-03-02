3cf1ba96ff26c81e3c87e5aea3a6aed48bfbc6ed
surendra-chouhan <chouhansurendra028@gmail.com>
2021-03-02 17:00:20 +0530

    Added Code to validate Mobile Number

--- regexProblems.java
+++ regexProblems.java
@@ -5,12 +5,13 @@ public class regexProblems{
 	String firstName;
 	String lastName;
 	String email;
+	String mobileNumber;
 
 	Scanner sc = new Scanner(System.in);
 
 	public void validateFirstName(){
 		System.out.println("\nEnter First Name (It should start with Capital Letter and have atleast 3 letters)");
-		firstName = sc.next();
+		firstName = sc.nextLine();
 
 		boolean validFirstName = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(firstName).matches();
 		if(validFirstName == true){
@@ -24,7 +25,7 @@ public class regexProblems{
 
 	public void validateLastName(){
 		System.out.println("\nEnter Last Name (It should start with Capital Letter and have atleast 3 letters)");
-		lastName = sc.next();
+		lastName = sc.nextLine();
 
 		boolean validLastName = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
 		if(validLastName == true){
@@ -38,7 +39,7 @@ public class regexProblems{
 
 	public void validateEmail(){
 		System.out.println("\nEnter Emaild ID : ");
-		email = sc.next();
+		email = sc.nextLine();
 
 		boolean validEmail = Pattern.compile("^[a-zA-Z0-9]+(?:[\\+._-][a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z]{1,}+)*$").matcher(email).matches();
 
@@ -50,11 +51,28 @@ public class regexProblems{
 		}
 	}
 
+	public void validateMobileNumber(){
+		System.out.println("\nEnter Phone Number : ");
+		mobileNumber = sc.nextLine();
+		System.out.println(mobileNumber);
+
+		boolean validMobileNumber = Pattern.compile("^[0-9]{2}[ ][0-9]{10}$").matcher(mobileNumber).matches();
+
+		if(validMobileNumber == true){
+			System.out.println("\nMobile Number is valid");
+		}
+		else{
+			System.out.println("\nMobile Number is not valid");
+		}
+
+	}
+
 	public static void main(String args[]){
 		regexProblems user = new regexProblems();
 
 		user.validateFirstName();
 		user.validateLastName();
 		user.validateEmail();
+		user.validateMobileNumber();
 	}
 }
